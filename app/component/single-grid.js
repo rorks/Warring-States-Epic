efbff2d38f9595a83a3d4e99cb6fc2bb6199f98b
byn9826 <byn9826@gmail.com>
2018-01-01 22:31:08 -0500

    set up cities grid

--- app/component/single-grid.js
+++ app/component/single-grid.js
@@ -5,7 +5,8 @@ Vue.component("single-grid", {
       class="single-location"
       v-bind:style="{
         borderColor: checkGridActive(code) ? 'lightgrey' : 'transparent',
-        backgroundColor: typeof cities[code] !== 'undefined' ? getStatesInfo()[cities[code].state].color : 'transparent'
+        backgroundColor: typeof cities[code] !== 'undefined' ? getStatesInfo()[cities[code].state].color : 'transparent',
+				opacity: typeof cities[code] !== 'undefined' && cities[code].state !== 0 ? 1 : 0.9
       }"
     >
 			<template v-if="typeof cities[code] !== 'undefined'">
--- app/predefine/general.js
+++ app/predefine/general.js
@@ -1,10 +1,5 @@
 Vue.mixin({
   methods: {
-    //return current langauge in use
-    getActiveLanguage: function() {
-      var available = [zh];
-      return available[0];
-    },
     //return true for active grid on map
     checkGridActive: function(code) {
       var inactive = [
@@ -26,13 +21,56 @@ Vue.mixin({
     //return cities info
     getCitiesInfo: function() {
       return {
-        '2-6': {name: '云中西'}
+        '2-8': {name: '云中'},
+				'3-10': {name: '雁门'},
+				'2-11': {name: '代郡'},
+				'4-9': {name: '太原'},
+				'5-12': {name: '邯郸'},
+				'2-14': {name: '上谷'},
+				'3-13': {name: '灵寿'},
+				'3-16': {name: '蓟'},
+				'2-18': {name: '辽西'},
+				'2-20': {name: '辽东'},
+				'6-2': {name: '陇西'},
+				'3-6': {name: '义渠'},
+				'9-2': {name: '蜀'},
+				'10-4': {name: '巴'},
+				'7-5': {name: '咸阳'},
+				'5-8': {name: '河东'},
+				'5-10': {name: '上党'},
+				'6-11': {name: '濮阳'},
+				'6-9': {name: '长平'},
+				'7-12': {name: '大梁'},
+				'4-14': {name: '饶安'},
+				'5-15': {name: '临淄'},
+				'6-14': {name: '定陶'},
+				'6-16': {name: '莒'},
+				'5-18': {name: '即墨'},
+				'6-7': {name: '渑池'},
+				'8-7': {name: '商'},
+				'8-9': {name: '安邑'},
+				'9-10': {name: '郑'},
+				'9-12': {name: '陈'},
+				'8-11': {name: '洛阳'},
+				'10-7': {name: '郢'},
+				'11-10': {name: '南阳'},
+				'11-13': {name: '寿春'},
+				'8-16': {name: '下邳'},
+				'10-18': {name: '吴'}
       }
     },
     //return states info
     getStatesInfo: function() {
       return [
-        {code: 0, color: 'DarkGrey'}
+        {code: 0, name: '野', color: 'DarkGrey'},
+				{code: 1, name: '齐', color: 'DarkOrange'},
+				{code: 2, name: '楚', color: 'DarkGreen'},
+				{code: 3, name: '燕', color: 'DarkPurple'},
+				{code: 4, name: '赵', color: 'DarkRed'},
+				{code: 5, name: '韩', color: 'Chocolate'},
+				{code: 6, name: '魏', color: 'DarkBlue'},
+				{code: 7, name: '秦', color: 'Black'},
+				{code: 8, name: '周', color: 'RosyBrown'}
       ]
     }
   }
